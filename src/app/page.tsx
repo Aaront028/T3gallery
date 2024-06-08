543674cd2c327cdc8763f406ddd8c3261ff462ba
Aaron T <info@aarontan.co.nz>
2024-06-08 16:49:36 +1200

    WIP - Using Next/Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
@@ -11,10 +12,17 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="w-48">
-          <img src={image.url} alt={`Image ${image.id}`} className="w-48 h-48 object-cover" />
+        <div key={image.id} className="flex w-48 h-48 flex-col">
+          <Image
+            src={image.url}
+            width={192}
+            height={192}
+            style={{ objectFit: "contain" }}
+            alt={image.name}
+
+          />
         </div>
 
       ))}
