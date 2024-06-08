d0b6591d23bec6454af3a061eb994353246fdcb3
Aaron T <info@aarontan.co.nz>
2024-06-08 13:03:45 +1200

    WIP - Setup database and link post to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/eeb457f6-3525-445c-b974-cdec580d4412-aipppd.jpg",
@@ -13,13 +14,19 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt={`Image ${image.id}`} className="w-48 h-48 object-cover" />
+        {posts.map(post => (<div key={post.id} className="w-48">{post.name}</div>))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
+            <img src={image.url} alt={`Image ${image.id + "-" + index}`} className="w-48 h-48 object-cover" />
           </div>
 
         ))}
